421f323e341c97699bfb1c741b4c0165cdbbc131
Guilherme Ribeiro <guilherme.ribeiro.souza@hotmail.com>
2026-05-24 17:56:53 -0300

    CORS ATT

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import authRoutes from './routes/authRoutes'
 
 dotenv.config()
@@ -7,6 +8,11 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors({
+  origin: 'https://api.potecheio.site',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 app.use(express.json())
 app.use('/auth', authRoutes)
 
